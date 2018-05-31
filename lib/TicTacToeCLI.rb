80199c1d971a78ab8a38a5aef591107a502b580b
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 17:20:52 +0000

    automatically backed up by learn

--- lib/TicTacToeCLI.rb
+++ lib/TicTacToeCLI.rb
@@ -12,7 +12,16 @@ class TicTacToeCLI
      "Please choose the following: 0, 1, or 2 players.\r\n".each_char do |c| #sleep 0.1
        print c
     end
-    player = gets.strip
+    input = gets.strip
+
+    if input == "1"
+      Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new).play
+    else
+      Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new).play
+    end
+
   end
+
+
 
 end
