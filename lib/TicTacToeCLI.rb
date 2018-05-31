4af19ed3daec479192418ea343f2941db2ef4f61
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 17:28:54 +0000

    automatically backed up by learn

--- lib/TicTacToeCLI.rb
+++ lib/TicTacToeCLI.rb
@@ -16,6 +16,7 @@ class TicTacToeCLI
 
     if input == "2"
       Game.new(Players::Human.new("X"), Players::Human.new("O"), Board.new).play
+      @board.display
     end
     #   Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new).play
     # else
