d648fc8ca24b48fa54587b6a97ab9967a69f2b65
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 20:58:11 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -87,7 +87,7 @@ class Game
       end
       sleep(@timer*1.5)
       # @timer -= (@timer/3)
-      end
+      # end
     puts "This round had #{x} wins for X, #{o} wins for O, and #{draw} draws."
     puts "A STRANGE GAME. THE ONLY WINNING MOVE IS NOT TO PLAY."
     puts "HOW ABOUT A NICE GAME OF CHESS?"
