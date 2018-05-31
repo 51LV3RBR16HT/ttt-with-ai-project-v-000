a3e5d882b6c2a92de3b21f54c520dd5375480aa4
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 17:37:00 +0000

    automatically backed up by learn

--- lib/TicTacToeCLI.rb
+++ lib/TicTacToeCLI.rb
@@ -1,5 +1,6 @@
 class TicTacToeCLI
 
+@board.display
   def call
     "Greetings, Professor Falken.\r\n".each_char do |c| #sleep 0.1
       print c
@@ -12,7 +13,7 @@ class TicTacToeCLI
      "Please choose the following: 0, 1, or 2 players.\r\n".each_char do |c| #sleep 0.1
        print c
     end
-      @board.display
+
     # input = gets.strip
     #
     # if input == "2"
