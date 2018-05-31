f8eeab3e9489ed27aa48f6b2368f996c9f98479d
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 21:30:49 +0000

    automatically backed up by learn

--- lib/TTTCLI.rb
+++ lib/TTTCLI.rb
@@ -12,9 +12,9 @@ def call
   elsif input == "2"
     game = Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new)
     game
-  elsif input == "3"
-    game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
-    game
+  # elsif input == "3"
+  #   game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new)
+  #   game
   else input == "wargames"
     game = Game.new(Player::Computer.new("X"), Player::Computer.new("O"), Board.new, true).wargames
   end
