7cc332747c17af98218a9e49be106aef25beea05
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 19:25:30 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
 
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      valid_moves.detect{|move|board.valid_move?(move)}
+      # valid_moves.detect{|move|board.valid_move?(move)}
     end
   end
 
