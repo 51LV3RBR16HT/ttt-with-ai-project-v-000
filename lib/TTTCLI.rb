a0d2bd31f883b97826dc3dfe05ef54d8ec658c33
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 21:46:19 +0000

    automatically backed up by learn

--- lib/TTTCLI.rb
+++ lib/TTTCLI.rb
@@ -2,8 +2,9 @@ class TTTCLI
 
 def call
   puts "Welcome to Tic Tac Toe!"
-  puts "Please choose:
   
+  puts "Please choose:
+
   [1] Player vs Player
   [2] Player vs Computer
   [3] Computer vs Computer"
