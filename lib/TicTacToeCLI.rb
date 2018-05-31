28e0b053313b6ae9a439c92a508de7fe6fed4fb5
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 17:09:26 +0000

    automatically backed up by learn

--- lib/TicTacToeCLI.rb
+++ lib/TicTacToeCLI.rb
@@ -2,16 +2,16 @@ class TicTacToeCLI
 
   def call
     "Greetings, Professor Falken.\r\n"#.each_char do |c| sleep 0.1
-      print c
-    end
+    #   print c
+    # end
     sleep 1
     "Shall we play a game of Tic Tac Toe?\r\n"#.each_char do |c| sleep 0.1
       print c
-    end
-    sleep 1
+    # end
+    # sleep 1
     "Please choose the following: 0, 1, or 2 players.\r\n"#.each_char do |c| sleep 0.1
-      print c
-    end
+    #   print c
+    # end
     player = gets.strip
   end
 
