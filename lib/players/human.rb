69821dd50942d87a1a35c9add90483d92064c6fc
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 21:44:54 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,12 +2,11 @@ module Players
   class Human < Player
 
     def move(board)
-
       puts "Please make your selection."
         board.display
         input = gets
         input
-
       end
     end
+    
 end
