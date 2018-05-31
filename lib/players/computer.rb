2d06103bb3f1978cae54e0ead4ed1b3b56f7a4da
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 19:59:09 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -8,8 +8,3 @@ module Players
   end
 
 end
-# This is where the AI goes.
-
-# build out this class
-
-# You should strive to build computer logic that when the computer plays, the game is unwinnable. You can hardcode your logic, things like "On turn 1 always try to go in the middle if you can" and if not "try to go in a corner" or any logic tree you can think of - there is an algorithm called Min/Max, but it's going to be hard to implement given our current implementation of a Game, so we recommend building something that's a more colloquial or condition-based algorithm.
