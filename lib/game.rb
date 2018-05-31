2357d9a10498befc4cb59f3e5bc28ebccef69547
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-31 21:19:08 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -92,13 +92,14 @@ class Game
       elsif winner == "O"
         o += 1
       end
+      puts "A STRANGE GAME. THE ONLY WINNING MOVE IS NOT TO PLAY."
+      puts "HOW ABOUT A NICE GAME OF CHESS?"
       # sleep(@timer*1.5)
       # @timer -= (@timer/3)
       # end
 
   end
-  puts "A STRANGE GAME. THE ONLY WINNING MOVE IS NOT TO PLAY."
-  puts "HOW ABOUT A NICE GAME OF CHESS?"
+  
 end
 puts "test"
 end
